a91ed01768e3e132796cae7c42a96a8f2ae4db3c
Rob Fairclough <rob.a.fairclough@gmail.com>
2019-02-18 12:03:23 +0000

    initialise firebase with serviceaccount

---
+++ server.js
@@ -0,0 +1,13 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./service-account.json');
+
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+
+const getTenants = async () => {
+  const tenants = await admin
+    .firestore()
+    .collection('tenants')
+    .get();
+  return tenants.docs.map(tenant => tenant.data());
+};
