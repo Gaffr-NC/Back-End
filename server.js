8282ebdb4638b7783a584d414eaf3b260c195b1a
Rob Fairclough <35062859+RobFairclough@users.noreply.github.com>
2019-02-18 19:19:50 +0000

    Firestore setup (#9)
    * initialise firebase with serviceaccount
    
    * basic get functions for firestore
    
    * add tenant function
    
    * add match function
    
    * add match function
    
    * block match function
    
    * delete functions
    
    * move database functions to utils folder
    
    * fix path of serviceaccount
    
    * use faker to generate tenants and landlords
    
    * remove unnecessary dependencies

---
+++ server.js
@@ -0,0 +1,40 @@
+const tenant = {
+  email: 'hello@email.com',
+  name: 'mr house man',
+  phone: '07916005655',
+  preferences: {
+    bedrooms: 1,
+    city: 'london',
+    maxPrice: 400,
+    minPrice: 0,
+    petsAllowed: true
+  }
+};
+
+const landlord = {
+  email: 'lucy@lord.net',
+  name: 'lucy Land',
+  phone: '0161726676786',
+  property: {
+    images: [
+      'https://urbangauge.com/wp-content/uploads/2018/06/flat.jpg',
+      'https://www.rightmove.co.uk/news/wp-content/uploads/2018/03/61682_3995232_IMG_01_0000.jpg'
+    ],
+    description: 'a flat',
+    preferences: {
+      bedrooms: 1,
+      city: 'london',
+      petsAllowed: true,
+      price: 400,
+      propertyType: 'flat'
+    }
+  }
+};
+
+// blockMatch('13JfwO1SNvwMLeGg1Wph');
+
+// deleteLandlord('ujKvwwT8ylEiHwItDMbI');
+
+// addLandlord(landlord);
+
+// deleteTenant('1');
