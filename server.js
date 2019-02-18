f7e7358d209fbaf91750defc1f1234cae0ce8176
Rob Fairclough <rob.a.fairclough@gmail.com>
2019-02-18 17:22:18 +0000

    add basic typedefs and graphql fetches

--- server.js
+++ server.js
@@ -1,7 +1,91 @@
-// blockMatch('13JfwO1SNvwMLeGg1Wph');
+const { ApolloServer, gql } = require('apollo-server');
+const admin = require('firebase-admin');
 
-// deleteLandlord('ujKvwwT8ylEiHwItDMbI');
+const serviceAccount = require('./service-account.json');
 
-// addLandlord(landlord);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+const typeDefs = gql`
+  type Preferences {
+    bedrooms: Int!
+    city: String
+    maxPrice: Int
+    minPrice: Int
+    smokingAllowed: Boolean
+    petsAllowed: Boolean
+  }
+  #tenant
+  type Tenant {
+    name: String
+    email: String
+    phone: String
+    preferences: Preferences
+  }
+  #landlord
+  type Property {
+    bedrooms: Int
+    description: String
+    images: [String]
+  }
+  type Landlord {
+    name: String
+    email: String
+    phone: String
+    property: Property
+  }
+  #match
+  type ChatMessage {
+    speaker: String
+    message: String
+  }
+  type Match {
+    tenantId: String
+    landlordId: String
+    chatHistory: [ChatMessage]
+  }
 
-// deleteTenant('1');
+  type Query {
+    matches: [Match]
+    tenants: [Tenant]
+    landlords: [Landlord]
+    tenant(id: String!): Tenant
+    landlord(id: String!): Landlord
+  }
+`;
+
+const resolvers = {
+  Query: {
+    async tenants() {
+      const tenants = await admin
+        .firestore()
+        .collection('tenants')
+        .get();
+      return tenants.docs.map(tenant => tenant.data());
+    },
+    async landlords() {
+      const landlords = await admin
+        .firestore()
+        .collection('landlords')
+        .get();
+      return landlords.docs.map(landlord => landlord.data());
+    },
+    async matches() {
+      const matches = await admin
+        .firestore()
+        .collection('matches')
+        .get();
+      return matches.docs.map(match => match.data());
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+});
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
