52cba3e3c622b4e3f458f288f11569bbafc50138
Rob Fairclough <35062859+RobFairclough@users.noreply.github.com>
2019-02-19 16:47:46 +0000

    Hosting gql for testing in frontend (#21)
    * initialise eslint, husky, package.json
    
    * initialise firebase with serviceaccount
    
    * basic get functions for firestore
    
    * add tenant function
    
    * add match function
    
    * add match function
    
    * block match function
    
    * delete functions
    
    * move database functions to utils folder
    
    * fix path of serviceaccount
    
    * use faker to generate tenants and landlords
    
    * remove unnecessary dependencies
    
    * add basic tests for faker functions
    
    * update husky hook
    
    * add basic typedefs and graphql fetches
    
    * Firestore setup (#9)
    
    * initialise firebase with serviceaccount
    
    * basic get functions for firestore
    
    * add tenant function
    
    * add match function
    
    * add match function
    
    * block match function
    
    * delete functions
    
    * move database functions to utils folder
    
    * fix path of serviceaccount
    
    * use faker to generate tenants and landlords
    
    * remove unnecessary dependencies
    
    * add id to queries, utils
    
    * add basic error handling for queries
    
    * Setup graphql queries (#13)
    
    * get tenant by id
    
    * lint changes
    
    * get landlord by id
    
    * lint changes
    
    * lint changes
    
    * add tenant
    
    * Add match (#14)
    
    * add landlord graphql query
    
    * make a match gql
    
    * Make utils reusable (#15)
    
    * add landlord graphql query
    
    * make a match gql
    
    * generalise util functions
    
    * Delete users (#16)
    
    * delete user gql query
    
    * add nodemon as dev dependency
    
    * Move graphql to new file (#17)
    
    * delete user gql query
    
    * add nodemon as dev dependency
    
    * move graphql setup to its own file
    
    * Update users (#18)
    
    * update user contact details
    
    * update landlord contact details
    
    * move typedefs and resolvers to own file
    
    * update tenant preferences
    
    * Update users (#19)
    
    * update user contact details
    
    * update landlord contact details
    
    * move typedefs and resolvers to own file
    
    * update tenant preferences
    
    * write first attempt at suitable landlords query
    
    * setup-for-heroku (#20)

---
+++ utils/index.js
@@ -0,0 +1,172 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('../service-account.json');
+
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+
+const getUsers = async (table) => {
+  const users = await admin
+    .firestore()
+    .collection(table)
+    .get();
+  return users.docs.map(user => ({ ...user.data(), id: user.id }));
+};
+
+const getUserById = async (id, table) => {
+  const user = await admin
+    .firestore()
+    .doc(`${table}/${id}`)
+    .get();
+  return user.data();
+};
+
+// ! Not finished
+// TODO: THIS
+const getSuitableLandlords = async (preferences) => {
+  console.log(preferences);
+  const {
+    smokingAllowed,
+    petsAllowed,
+    minPrice,
+    maxPrice,
+    city,
+    bedrooms,
+    propertyType,
+  } = preferences;
+
+  let landlords = await admin.firestore().collection('landlords');
+  if (smokingAllowed) {
+    landlords = landlords.where('property.smokingAllowed', '==', true);
+  }
+  if (petsAllowed) {
+    landlords = landlords.where('property.petsAllowed', '==', true);
+  }
+  if (minPrice || maxPrice) {
+    landlords = landlords
+      .where('property.price', '>=', minPrice || 0)
+      .where('property.price', '<=', maxPrice || Infinity);
+  }
+  if (bedrooms) {
+    landlords = landlords.where('property.bedrooms', '>=', bedrooms);
+  }
+  if (city) {
+    landlords = landlords.where('property.city', '==', city);
+  }
+  if (propertyType) {
+    landlords = landlords.where('property.propertyType', '==', propertyType);
+  }
+  landlords = await landlords.get();
+  return landlords.docs.map(landlord => ({ ...landlord.data(), id: landlord.id }));
+};
+
+const getMatchesByLandlord = async (landlordId) => {
+  const matches = await admin
+    .firestore()
+    .collection('matches')
+    .where('landlordId', '==', landlordId)
+    .get();
+  return matches.docs.map(match => ({ ...match.data(), id: match.id }));
+};
+
+const getMatchesByTenant = async (tenantId) => {
+  const matches = await admin
+    .firestore()
+    .collection('matches')
+    .where('tenantId', '==', tenantId)
+    .get();
+  return matches.docs.map(match => ({ ...match.data(), id: match.id }));
+};
+
+// ID comes from auth as UID
+const addUser = async (id, user, table) => {
+  const userRef = await admin
+    .firestore()
+    .collection(table)
+    .doc(id)
+    .set(user);
+  console.log(`added user to ${table} with id: ${id}`);
+  return userRef;
+};
+
+const addMatch = async (landlordId, tenantId) => {
+  await admin
+    .firestore()
+    .collection('matches')
+    .add({
+      landlordId,
+      tenantId,
+      chatHistory: [],
+      blocked: false,
+    })
+    .then((ref) => {
+      console.log('match made, id: ', ref.id);
+    });
+};
+
+const updateUserContact = async (id, user, table) => {
+  const userRef = await admin
+    .firestore()
+    .collection(table)
+    .doc(id);
+  await userRef.update(user);
+  return userRef.id;
+};
+const updateProperty = async (id, property) => {
+  const keys = Object.keys(property).map(key => `property.${key}`);
+  const values = Object.values(property);
+  const updatedObj = {};
+  keys.forEach((key, index) => {
+    updatedObj[key] = values[index];
+  });
+  const landlordRef = await admin
+    .firestore()
+    .collection('landlords')
+    .doc(id);
+  await landlordRef.update(updatedObj);
+  return landlordRef.id;
+};
+const updatePreferences = async (id, preferences) => {
+  const keys = Object.keys(preferences).map(key => `preferences.${key}`);
+  const values = Object.values(preferences);
+  const updatedObj = {};
+  keys.forEach((key, index) => {
+    updatedObj[key] = values[index];
+  });
+  const tenantRef = await admin
+    .firestore()
+    .collection('tenants')
+    .doc(id);
+  await tenantRef.update(updatedObj);
+  return tenantRef.id;
+};
+const blockMatch = async (matchId) => {
+  admin
+    .firestore()
+    .collection('matches')
+    .doc(matchId)
+    .update({ blocked: false });
+};
+
+const deleteUserById = async (id, table) => {
+  const value = await admin
+    .firestore()
+    .collection(table)
+    .doc(id)
+    .delete();
+  console.log(value);
+};
+
+module.exports = {
+  deleteUserById,
+  blockMatch,
+  addMatch,
+  addUser,
+  getUsers,
+  getMatchesByLandlord,
+  getMatchesByTenant,
+  getUserById,
+  getSuitableLandlords,
+  updateUserContact,
+  updateProperty,
+  updatePreferences,
+};
