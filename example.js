242187ff4751e06be3b614fd2f2e4d4638437c8b
Natali <43702730+nataliaelef@users.noreply.github.com>
2019-02-20 14:28:46 +0000

    Typescript conversion (#24)
    * first ts function
    
    * typescript conversion
    
    * typescript conversion

--- example.js
+++ example.js
@@ -1,47 +1,12 @@
-/* eslint-disable camelcase */
 const admin = require('firebase-admin');
-
-const SERVICE_ACCOUNT = process.env.NODE_ENV !== 'production' ? require('../service-account.json') : '';
-
-if (process.env.NODE_ENV === 'production') {
-  const {
-    type,
-    project_id,
-    private_key_id,
-    private_key,
-    client_email,
-    client_id,
-    auth_uri,
-    token_uri,
-    auth_provider_x509_cert_url,
-    client_x509_cert_url,
-  } = process.env;
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      type,
-      project_id,
-      private_key_id,
-      private_key: private_key.replace(/\\n/g, '\n'),
-      client_email,
-      client_id,
-      auth_uri,
-      token_uri,
-      auth_provider_x509_cert_url,
-      client_x509_cert_url,
-    }),
-  });
-} else {
-  admin.initializeApp({ credential: admin.credential.cert(SERVICE_ACCOUNT) });
-}
 
 const getUsers = async (table) => {
   const users = await admin
     .firestore()
     .collection(table)
     .get();
-  return users.docs.map(user => ({ ...user.data(), id: user.id }));
+  return users.docs.map(user => Object.assign({}, user.data(), { id: user.id }));
 };
-
 const getUserById = async (id, table) => {
   const user = await admin
     .firestore()
@@ -49,65 +14,22 @@ const getUserById = async (id, table) => {
     .get();
   return user.data();
 };
-
-// ! Not finished
-// TODO: THIS
-const getSuitableLandlords = async (preferences) => {
-  console.log(preferences);
-  const {
-    smokingAllowed,
-    petsAllowed,
-    minPrice,
-    maxPrice,
-    city,
-    bedrooms,
-    propertyType,
-  } = preferences;
-
-  let landlords = await admin.firestore().collection('landlords');
-  if (smokingAllowed) {
-    landlords = landlords.where('property.smokingAllowed', '==', true);
-  }
-  if (petsAllowed) {
-    landlords = landlords.where('property.petsAllowed', '==', true);
-  }
-  if (minPrice || maxPrice) {
-    landlords = landlords
-      .where('property.price', '>=', minPrice || 0)
-      .where('property.price', '<=', maxPrice || Infinity);
-  }
-  if (bedrooms) {
-    landlords = landlords.where('property.bedrooms', '>=', bedrooms);
-  }
-  if (city) {
-    landlords = landlords.where('property.city', '==', city);
-  }
-  if (propertyType) {
-    landlords = landlords.where('property.propertyType', '==', propertyType);
-  }
-  landlords = await landlords.get();
-  return landlords.docs.map(landlord => ({ ...landlord.data(), id: landlord.id }));
-};
-
 const getMatchesByLandlord = async (landlordId) => {
   const matches = await admin
     .firestore()
     .collection('matches')
     .where('landlordId', '==', landlordId)
     .get();
-  return matches.docs.map(match => ({ ...match.data(), id: match.id }));
+  return matches.docs.map(match => Object.assign({}, match.data(), { id: match.id }));
 };
-
 const getMatchesByTenant = async (tenantId) => {
   const matches = await admin
     .firestore()
     .collection('matches')
     .where('tenantId', '==', tenantId)
     .get();
-  return matches.docs.map(match => ({ ...match.data(), id: match.id }));
+  return matches.docs.map(match => Object.assign({}, match.data(), { id: match.id }));
 };
-
-// ID comes from auth as UID
 const addUser = async (id, user, table) => {
   const userRef = await admin
     .firestore()
@@ -117,7 +39,6 @@ const addUser = async (id, user, table) => {
   console.log(`added user to ${table} with id: ${id}`);
   return userRef;
 };
-
 const addMatch = async (landlordId, tenantId) => {
   await admin
     .firestore()
@@ -132,7 +53,6 @@ const addMatch = async (landlordId, tenantId) => {
       console.log('match made, id: ', ref.id);
     });
 };
-
 const updateUserContact = async (id, user, table) => {
   const userRef = await admin
     .firestore()
@@ -141,7 +61,6 @@ const updateUserContact = async (id, user, table) => {
   await userRef.update(user);
   return userRef.id;
 };
-
 const updateProperty = async (id, property) => {
   const keys = Object.keys(property).map(key => `property.${key}`);
   const values = Object.values(property);
@@ -156,7 +75,6 @@ const updateProperty = async (id, property) => {
   await landlordRef.update(updatedObj);
   return landlordRef.id;
 };
-
 const updatePreferences = async (id, preferences) => {
   const keys = Object.keys(preferences).map(key => `preferences.${key}`);
   const values = Object.values(preferences);
@@ -171,7 +89,6 @@ const updatePreferences = async (id, preferences) => {
   await tenantRef.update(updatedObj);
   return tenantRef.id;
 };
-
 const blockMatch = async (matchId) => {
   admin
     .firestore()
@@ -179,7 +96,6 @@ const blockMatch = async (matchId) => {
     .doc(matchId)
     .update({ blocked: false });
 };
-
 const deleteUserById = async (id, table) => {
   const value = await admin
     .firestore()
@@ -189,17 +105,16 @@ const deleteUserById = async (id, table) => {
   console.log(value);
 };
 
-module.exports = {
-  deleteUserById,
-  blockMatch,
-  addMatch,
-  addUser,
+export {
   getUsers,
+  getUserById,
   getMatchesByLandlord,
   getMatchesByTenant,
-  getUserById,
-  getSuitableLandlords,
+  addUser,
+  addMatch,
   updateUserContact,
   updateProperty,
   updatePreferences,
+  blockMatch,
+  deleteUserById,
 };
